be19e0f4e5d677d3f6b37e52c377404a1c2803d0
Forrest Bicker <bickerforrest@gmail.com>
2021-03-10 10:02:34 -0500

    add sample files

---
+++ themes/demo/java.java
@@ -0,0 +1,17 @@
+package demo;
+
+import java.util.*;
+
+public class java<T> {
+
+	private static int CONSTANT = 4;
+	double pie = 4;
+
+	public java() {
+		this.pie = Math.PI;
+	}
+
+	private Set<T> setUnion(Set<T> set1, Set<T> set2) {
+		return null;
+	}
+}
